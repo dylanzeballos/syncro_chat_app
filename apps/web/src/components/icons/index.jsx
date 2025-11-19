ee25a5dc894a77bbb295dbb66a69ea3e28de7133
dylanzeballos <zeballosperedodylanamin@gmail.com>
2025-11-19 05:15:04 -0400

    feat(web): add SVG icon components library

---
+++ apps/web/src/components/icons/index.jsx
@@ -0,0 +1,13 @@
+// Central export file for all icons
+export { GoogleIcon } from "./GoogleIcon";
+export { EmailIcon } from "./EmailIcon";
+export { LockIcon } from "./LockIcon";
+export { UserIcon } from "./UserIcon";
+export { SendIcon } from "./SendIcon";
+export { LogoutIcon } from "./LogoutIcon";
+export { ChatIcon } from "./ChatIcon";
+export { MenuIcon } from "./MenuIcon";
+export { CloseIcon } from "./CloseIcon";
+export { PlusIcon } from "./PlusIcon";
+export { LoadingSpinner } from "./LoadingSpinner";
+export { HamburgerIcon } from "./HamburgerIcon";
